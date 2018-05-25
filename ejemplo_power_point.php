234ea4a0b260ae9d357791ce7a6eeee5fb2cb75c
yeison aristizabal <kaos34k@gmail.com>
2018-05-25 12:10:19 -0500

    agregar ejemplo para el remplazo de imagenes

--- ejemplo_power_point.php
+++ ejemplo_power_point.php
@@ -1,10 +1,11 @@
 <?php
 /**
 * http://php.net/manual/es/book.zip.php
+* lo archivos pptx pueden ser tratados
 */
 
 // Defino un nombre único para el directorio de trabajo y lo creo
-$directorio = '/tmp/talento_temporales/' . hash('sha256', rand() ) . '/';
+$directorio = '/tmp/temporales/' . hash('sha256', rand() ) . '/';
 mkdir( $directorio, 0777, true );
 
 // Copio el zip
@@ -16,13 +17,27 @@ $zip = new ZipArchive;
 $res = $zip->open( $zip_temporal );
 if ($res === TRUE) {
        // Obtengo el slide
-       $zip->extractTo( $directorio, array( 'ppt/slides/slide1.xml' ) );
+       $zip->extractTo( $directorio, array( 'ppt/slides/slide1.xml', 'ppt/media/mi_imagen.png' ) );
+       
+       //tambien se puede hacer el remplazo de imagenes, 
+       //para eso debe tener en cuenta el nombre y tipo del archivo que se desea rempazar
+       //$fichero = 'ppt/media/mi_imagen.png';
+       //unlink($directorio .$fichero );
+       //copy('la nueva imagen', $directorio .$fichero);
+       //guardo el nuevo archivo en el zip
+       //$zip->addFile($directorio .$fichero, 'ppt/media/image6.png' );
+       
+       //cargo la informacion del archivo que deseo modificar
        $slide1 = file_get_contents( $directorio . 'ppt/slides/slide1.xml' );
        //busco el texto que y remplazo
        $slide1 = str_replace( 'Nombre Completo: ', 'Mi nombre completo', $slide1 );
+       
        //luego actualizo el zip con el nuevo contenido 
        $zip->addFromString('ppt/slides/slide1.xml', $slide1 );
+       
+       //cierro el archivo
        $zip->close();
+       
 
 } else {
        echo 'failed';
@@ -31,7 +46,7 @@ if ($res === TRUE) {
 // Entrego el archivo
 header('Content-Description: File Transfer');
 header('Content-type: application/vnd.openxmlformats-officedocument.presentationml.presentation' );
-header('Content-Disposition: attachment; filename=""Ficha de Colaborador.pptx""');
+header('Content-Disposition: attachment; filename=""ficha.pptx""');
 header('Expires: 0');
 header('Cache-Control: must-revalidate');
 header('Pragma: public');
